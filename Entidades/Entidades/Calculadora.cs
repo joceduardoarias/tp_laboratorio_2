59dc088f2d46593380b24a2775b7b28deb94b29b
Jose Arias Melo <36265049+joceduardoarias@users.noreply.github.com>
2019-04-21 19:40:21 -0300

    Correcion
    Fallo en uso del la consola GIT subio una version vieja del TP, practicar subida por GIT para no cometer el mism error!!!

--- Entidades/Entidades/Calculadora.cs
+++ Entidades/Entidades/Calculadora.cs
@@ -7,11 +7,7 @@ using System.Threading.Tasks;
 namespace Entidades
 {
     public static class Calculadora
-    {   /// <summary>
-        /// Deberá validar que el operador recibido sea +, -, / o *. Caso contrario retornará +.
-        /// </summary>
-        /// <param name="operador"></param>
-        /// <returns>eperador binario</returns>
+    {  
         private static string ValidarOperador(string operador)
         {
 
@@ -35,22 +31,11 @@ namespace Entidades
             }
             return operador;
         }
-        /// <summary>
-        /// Validará y realizará la operación pedida entre ambos números.
-        /// Si se tratara de una división por 0, retornará double.MinValue.
-        /// </summary>
-        /// <param name="n1"></param>
-        /// <param name="n2"></param>
-        /// <param name="operador"></param>
-        /// <returns></returns>
+       
         public static double Operar(Numero n1, Numero n2, string operador)
         {
             double retorno = 0;
-            string aux = n2.ToString();
-            if (aux == "0" && ValidarOperador(operador) == "/")// validar que sea igual a cero y operador "/"
-            {
-                return Double.MinValue;
-            }
+
             switch (ValidarOperador(operador))
             {
                 case "+":
@@ -64,6 +49,12 @@ namespace Entidades
                     break;
                 case "/":
                     retorno = n1 / n2;
+                    if (double.IsInfinity(retorno))
+                    {
+                        retorno = double.MinValue;
+                    }
+                    break;
+                default:
                     break;
             }
             return retorno;
---
+++ Entidades/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private static double Operate(string n1, string n2, string operador)
+        {
+
+            Numero numeroUno;
+            numeroUno = new Numero(n1);
+            Numero numeroDos;
+            numeroDos = new Numero(n2);
+            double retorno;
+            retorno = Calculadora.Operar(numeroUno, numeroDos, operador);
+            return retorno;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Operar_Click(object sender, EventArgs e)
+        {
+            SalidaRespuesta.Text = Operate(textBox1.Text, textBox2.Text, comboBox1.Text).ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("/");
+            comboBox1.Items.Add("*");
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            SalidaRespuesta.Text = " ";
+            comboBox1.Text = "";
+
+        }
+
+        private void ConvertDecimal_Click(object sender, EventArgs e)
+        {
+            Numero numero;
+            numero = new Numero();
+            SalidaRespuesta.Text = numero.BinarioDecimal(SalidaRespuesta.Text);
+        }
+
+        private void ConvertBinario_Click(object sender, EventArgs e)
+        {
+            Numero numero;
+            numero = new Numero();
+            string resultado = "";
+            resultado = numero.DecimalBinario(SalidaRespuesta.Text);
+            SalidaRespuesta.Text = resultado;
+        }     
+    }
+}
