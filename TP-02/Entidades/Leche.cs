95874fcf43002f72e0c9eb5af8882bf3b13ebb3c
Jose Arias Melo <36265049+joceduardoarias@users.noreply.github.com>
2019-05-14 21:42:59 -0300

    TP2 funciona correctamente.

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+   public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+            tipo = ETipo.Entera;
+        }
+        public Leche(EMarca marca,string patente,ConsoleColor color,ETipo tipo):this(marca,patente,color)
+        {
+            this.tipo = tipo;
+        }
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
